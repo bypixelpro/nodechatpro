5d61d246581e93391ad64b17b1e0f9efc0697cec
David <info@pixelpro.es>
2018-05-19 23:18:04 +0200

    chat init no mongodb

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var bp = require('body-parser')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+app.use(express.static(__dirname))
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: false }))
+
+var msg = []
+
+app.get('/msg', (request, respond) =>{
+    respond.send(msg)
+})
+app.post('/msg', (request, respond) =>{
+    console.log('Request', request.body)
+    msg.push(request.body)
+    io.emit('message', request.body)
+    
+    respond.sendStatus(200)
+})
+
+io.on('connection', (socket) => {
+    console.log('usuario conectado')
+})
+
+var server = http.listen(3000, ()=>{
+    console.log('Saliendo por el puerto', server.address().port)
+})
+
